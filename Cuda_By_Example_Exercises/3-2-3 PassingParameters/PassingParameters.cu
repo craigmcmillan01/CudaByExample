5f999b2e568101021cacf4726b68fd5c692d2e4c
Craig McMillan <10004794@napier.ac.uk>
2013-06-12 21:34:37 +0100

    Added event timer to 3-2-3 passing params

--- Cuda_By_Example_Exercises/3-2-3 PassingParameters/PassingParameters.cu
+++ Cuda_By_Example_Exercises/3-2-3 PassingParameters/PassingParameters.cu
@@ -18,7 +18,12 @@ int main(void)
 	int c; int a = 100; int b = 50;
 	int *dev_c;
 
-	/*
+	// capture the start time
+    cudaEvent_t     start, stop;
+    HANDLE_ERROR( cudaEventCreate( &start ) );
+    HANDLE_ERROR( cudaEventCreate( &stop ) );
+    HANDLE_ERROR( cudaEventRecord( start, 0 ) );
+/*
 	 Allocate memory on the device(GPU)
 	 param one is a pointer to a pointer you want to hold the address
 	 and param 2 is the size of the memory allocation.
@@ -32,6 +37,17 @@ int main(void)
 
 	//Copy the memory from the device to the host so the data can be used by the host
 	HANDLE_ERROR(cudaMemcpy(&c, dev_c, sizeof(int), cudaMemcpyDeviceToHost));
+
+	// get stop time, and display the timing results
+    HANDLE_ERROR( cudaEventRecord( stop, 0 ) );
+    HANDLE_ERROR( cudaEventSynchronize( stop ) );
+    float   elapsedTime;
+    HANDLE_ERROR( cudaEventElapsedTime( &elapsedTime,
+                                        start, stop ) );
+    printf( "Time to generate:  %3.1f ms\n", elapsedTime );
+
+    HANDLE_ERROR( cudaEventDestroy( start ) );
+    HANDLE_ERROR( cudaEventDestroy( stop ) );
 
 	//Print the result
 	printf("%d + %d = %d\n", a, b, c);
